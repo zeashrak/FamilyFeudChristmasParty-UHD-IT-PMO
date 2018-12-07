641690c3383be57c868902937ca80b99f2176e70
Eashrak Zubair <zeashrak@gmail.com>
2018-12-07 09:16:10 -0600

    Create timer test.js

---
+++ DEMO/timer test.js
@@ -0,0 +1,15 @@
+<script src="lib/easytimer/dist/easytimer.min.js"></script>
+
+<script>
+
+var timer = new Timer();
+timer.start({countdown: true, startValues: {seconds: 30}});
+$('#countdownExample .values').html(timer.getTimeValues().toString());
+timer.addEventListener('secondsUpdated', function (e) {
+    $('#countdownExample .values').html(timer.getTimeValues().toString());
+});
+timer.addEventListener('targetAchieved', function (e) {
+    $('#countdownExample .values').html('KABOOM!!');
+});
+</script>
+
