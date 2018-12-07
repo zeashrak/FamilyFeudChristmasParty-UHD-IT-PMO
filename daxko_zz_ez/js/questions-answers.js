df941c5e0e362aafb46e45e80052308ae7955246
Eashrak Zubair <zeashrak@gmail.com>
2018-12-07 09:12:43 -0600

    Create questions-answers.js

---
+++ daxko_zz_ez/js/questions-answers.js
@@ -0,0 +1,49 @@
+// questions must be less than 104 characters
+// answers must be less than 18 characters
+var data = 
+    [
+        [
+            // Question found on
+            // http://familyfeudfriends.arjdesigns.com/?Question=998
+            
+            "Name a state in America's New England.", 
+            [
+                'Maine', 28,
+                'Massachusetts', 26, 
+                'Vermont', 14, 
+                'Connecticut', 12, 
+                'Rhode Island', 5, 
+                'New Hampshire', 5
+            ]
+        ],
+
+        [
+            // Question found on
+            // http://familyfeudfriends.arjdesigns.com/?Question=4
+            
+            "Name a word that most people yell at their dogs.", 
+            [
+                'No', 27,
+                'Sit', 23,
+                'Stop', 14,
+                'Down', 7,
+                'Fetch', 6,
+                'Bad', 5
+            ]
+        ],
+        
+        [
+            // Question found on
+            // http://familyfeudfriends.arjdesigns.com/?Question=8
+            
+            "Name a bad sport for someone who is afraid of the water.", 
+            [
+                'Swimming', 33,
+                'Water Polo', 21,
+                'Water Skiing', 17,
+                'Diving', 11,
+                'Scuba', 6,
+                'Surfing', 5
+            ]
+        ]
+    ];
